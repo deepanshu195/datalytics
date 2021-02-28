a0fab0ec01246eed4a45c5abb64e22129fe79684
deepanshu195 <deepanshugarg195@gmail.com>
2021-02-28 19:13:45 +0530

    feat(setup): reducer and demo setup

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,34 @@
 import React from "react";
+import classes from "./Main.module.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+let images = ["logo192.png","logo192.png","logo192.png"]
+const LaptopCarousel = ()=>{
+    return(<div style={{position:"relative"}}>
+      <div>
+      <img className={classes.laptop_image} src="/laptop.png" />
+      </div>
+      <div className={classes.laptop_carousel_images_div}>      
+            <Carousel showThumbs={false} showArrows={true} autoPlay={true}  swipeable={true} showIndicators={true} infiniteLoop={true}> 
+          {
+              images.map((url, index) => (
+                  <div key={index}>
+                    <img className={classes.laptop_carousel_images} src={url} />
+                          <p>Legend</p>
+                  </div>
+              ))
+          }
+        </Carousel>
+      </div>
+    </div>)
+}
 
 const Main = (props) => {
   return (
     <React.Fragment>
-      <p>Main Content</p>
+      <div>Main Content</div>
+      <LaptopCarousel></LaptopCarousel>
     </React.Fragment>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { applyMiddleware, createStore } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { rootReducer } from "./store";
+import { Provider } from "react-redux";
 
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<Provider store={createStoreWithMiddleware(rootReducer)}>
+        		<App />
+    		</Provider>
 		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById("root")
