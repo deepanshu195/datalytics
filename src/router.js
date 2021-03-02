5162d9a09a5d17cc1cd6c1a2a2e179d5a0062e92
Sandeep-raj <gurusandeepraj@gmail.com>
2021-03-02 23:30:33 +0530

    Stepper Style

--- src/router.js
+++ src/router.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Demo from "./containers/Demo";
 import Landing from "./containers/LandingPage/LandingPage";
+import Stepper from "./containers/Stepper/Stepper";
 
 const Router = (props) => {
   return (
     <Switch>
       <Route path="/" exact component={Landing} />
       <Route path="/demo" component={Demo} />
+      <Route path="/stepper" component={Stepper} />
     </Switch>
   );
 };
