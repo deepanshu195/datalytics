979da8974ae5ec1d3338e5bd62f7a57ef353b445
deepanshu195 <deepanshugarg195@gmail.com>
2021-04-25 14:50:26 +0530

    feat(change): 3 done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,71 +1,110 @@
 import React from "react";
 import classes from "./Footer.module.css";
 
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 
 import { NavLink } from "react-router-dom";
 
 let footerNavLinks = [
   {
-    name:"About us",
-    link:"/"
+    name: "About us",
+    link: "/",
   },
   {
-    name:"Documentation",
-    link:"/"
-  },{
-    name:"Services",
-    link:"/"
-  },{
-    name:"Technologies",
-    link:"/"
-  },{
-    name:"Blogs",
-    link:"/"
-  },{
-    name:"Careers",
-    link:"/"
-  },{
-    name:"Contact Us",
-    link:"/"
-  },{
-    name:"Sitemap",
-    link:"/"
-  }
-]
+    name: "Documentation",
+    link: "/",
+  },
+  {
+    name: "Services",
+    link: "/",
+  },
+  {
+    name: "Technologies",
+    link: "/",
+  },
+  {
+    name: "Blogs",
+    link: "/",
+  },
+  {
+    name: "Careers",
+    link: "/",
+  },
+  {
+    name: "Contact Us",
+    link: "/",
+  },
+  {
+    name: "Sitemap",
+    link: "/",
+  },
+];
 
 const Footer = () => {
   let justify;
-  if(window.innerWidth < 960 ){
-    justify="center"
+  if (window.innerWidth < 960) {
+    justify = "center";
   }
   return (
     <React.Fragment>
       <div className={` ${classes.footer}`}>
-         <Grid container spacing={5} className={`${classes.tilted}`} style={{margin:"unset",width:"100vw"}}>           
-        {footerNavLinks.map((val,index)=>
-          <Grid item xs={12}  sm={4} md={3} container  spacing={0}  key={"footer_landing_page"+index}  justify={"center"}> 
-           
+        <Grid
+          container
+          spacing={5}
+          className={`${classes.tilted}`}
+          style={{ margin: "unset", width: "100vw" }}
+        >
+          {footerNavLinks.map((val, index) => (
+            <Grid
+              item
+              xs={12}
+              sm={4}
+              md={3}
+              container
+              spacing={0}
+              key={"footer_landing_page" + index}
+              justify={"center"}
+            >
               <NavLink
                 to={val.link}
                 draggable={false}
                 className={` ${classes.decoration} `}
               >
                 {val.name}
-            </NavLink>
-      </Grid>
-      )}
-      </Grid>
-      </div>  
-      <div className={classes.footer_logo_box}> 
-      <Grid  container spacing={5}  >
-        <Grid item xs={12}  sm={4} md={4} container  spacing={0}    justify={ justify || "flex-start"}>
-        {/* <img src={"https://res.cloudinary.com/datalytics/image/upload/v1615459421/Datalytics/logo_tljs0z.svg"} alt={"datalytics logo"}  style={{height:"100px"}}/> */}
+              </NavLink>
+            </Grid>
+          ))}
         </Grid>
-        <Grid item xs={12}  sm={6} md={6} container  spacing={0}    justify={ justify || "flex-start"} alignContent={"center"}>
-        &copy; 2020  Datalytics Technologies Pvt. Ltd.
+      </div>
+      <div className={classes.footer_logo_box}>
+        <Grid
+          container
+          spacing={5}
+          style={{ width: "100%", padding: "10px 0px" }}
+        >
+          <Grid
+            item
+            xs={12}
+            sm={4}
+            md={4}
+            style={{ width: "100%" }}
+            container
+            justify={justify || "flex-start"}
+          >
+            {/* <img src={"https://res.cloudinary.com/datalytics/image/upload/v1615459421/Datalytics/logo_tljs0z.svg"} alt={"datalytics logo"}  style={{height:"100px"}}/> */}
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={6}
+            container
+            justify={justify || "flex-start"}
+            alignContent={"center"}
+          >
+            &copy; 2020 Datalytics Technologies Pvt. Ltd.
+          </Grid>
         </Grid>
-      </Grid>
       </div>
     </React.Fragment>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,86 +1,127 @@
 import React from "react";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 import { NavLink } from "react-router-dom";
-import classes from  "./Header.module.css";
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import classes from "./Header.module.css";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& > *': {
-      margin: theme.spacing(0),       
+    "& > *": {
+      margin: theme.spacing(0),
     },
   },
   paper: {
     padding: theme.spacing(0),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+    textAlign: "center",
+    color: "white",
+    background: "inherit",
     // minWidth:"150px"
   },
-  button:{
-    background:"#25313C",
-    color:"white",
-    '&:hover': {
+  button: {
+    background: "#25313C",
+    color: "white",
+    "&:hover": {
       backgroundColor: "#25313C",
-      color: '#FFF'
+      color: "#FFF",
     },
-    fontSize:"22px",
-    lineHeight:"30px",
-    padding:"5px 10px",
-    fontWeight:"bold",
-    fontFamily:"Montserrat",
-    margin:"0px"
+    fontSize: "22px",
+    lineHeight: "30px",
+    padding: "5px 10px",
+    fontWeight: "bold",
+    fontFamily: "Montserrat",
+    margin: "0px",
   },
- 
 }));
 
 const Header = () => {
   const materialUiClasses = useStyles();
   let windowWidth = window.innerWidth;
-  let justifyItems ="flex-end";
-  if(windowWidth<960){
-    justifyItems="center";
+  let justifyItems = "flex-end";
+  if (windowWidth < 960) {
+    justifyItems = "center";
   }
   return (
     <div className={`${materialUiClasses.root} ${classes.header}`}>
-      
-  <Grid container spacing={1}>
-      <Grid item xs={12}  sm={12}   lg={2} >
+      <Grid container spacing={1}>
+        <Grid item xs={12} sm={12} lg={2}>
           <Paper className={`${materialUiClasses.paper} ${classes.boxShadow}`}>
-        <img src={"https://res.cloudinary.com/datalytics/image/upload/v1615459421/Datalytics/logo_tljs0z.svg"} alt={"logo"} style={{height:"89px"}}></img>
-      </Paper>
-      </Grid>
-      <Grid container  item xs={6} sm={4} md={2}     lg={2} justify={justifyItems} alignItems={"center"}>
-          <Paper className={`${materialUiClasses.paper} ${classes.boxShadow}`}>
-          <NavLink to={"/"} className={`${classes.navlinks}`}>
-        Demo
-          </NavLink>
-  
+            <a href="/">
+              <img
+                src={
+                  "https://res.cloudinary.com/datalytics/image/upload/v1619335327/Datalytics/Datalytics_Logo_2_mqeeyy.jpg"
+                }
+                alt={"logo"}
+                className={classes.header_logo}
+                // style={{ height: "89px" }}
+              ></img>
+            </a>
           </Paper>
         </Grid>
-        <Grid container  item xs={6} sm={4} md={2}    lg={2} justify={justifyItems} alignItems={"center"}>
+        <Grid
+          container
+          item
+          xs={6}
+          sm={4}
+          md={2}
+          lg={2}
+          justify={justifyItems}
+          alignItems={"center"}
+        >
           <Paper className={`${materialUiClasses.paper} ${classes.boxShadow}`}>
-          <NavLink to={"/"} className={`${classes.navlinks}`}>
-        Solutions
-          </NavLink>
-  
+            <NavLink to={"/"} className={`${classes.navlinks}`}>
+              Demo
+            </NavLink>
           </Paper>
         </Grid>
-        <Grid container  item xs={6} sm={4} md={2}    lg={2} justify={justifyItems} alignItems={"center"}>
+        <Grid
+          container
+          item
+          xs={6}
+          sm={4}
+          md={2}
+          lg={2}
+          justify={justifyItems}
+          alignItems={"center"}
+        >
           <Paper className={`${materialUiClasses.paper} ${classes.boxShadow}`}>
-          <NavLink to={"/"} className={`${classes.navlinks}`}>
-        Pricing
-          </NavLink>
-  
+            <NavLink to={"/"} className={`${classes.navlinks}`}>
+              Solutions
+            </NavLink>
           </Paper>
         </Grid>
-        <Grid container  item xs={6} sm={4} md={2}    lg={2} justify={justifyItems} alignItems={"center"}>
+        <Grid
+          container
+          item
+          xs={6}
+          sm={4}
+          md={2}
+          lg={2}
+          justify={justifyItems}
+          alignItems={"center"}
+        >
           <Paper className={`${materialUiClasses.paper} ${classes.boxShadow}`}>
-          <NavLink to={"/"} className={`${classes.navlinks}`}>
-        Services
-          </NavLink>
+            <NavLink to={"/"} className={`${classes.navlinks}`}>
+              Pricing
+            </NavLink>
+          </Paper>
+        </Grid>
+        <Grid
+          container
+          item
+          xs={6}
+          sm={4}
+          md={2}
+          lg={2}
+          justify={justifyItems}
+          alignItems={"center"}
+        >
+          <Paper className={`${materialUiClasses.paper} ${classes.boxShadow}`}>
+            <NavLink to={"/"} className={`${classes.navlinks}`}>
+              Services
+            </NavLink>
           </Paper>
         </Grid>
         {/* <Grid container  item xs={6} sm={2}    lg={2} justify={justifyItems} alignItems={"center"}>
@@ -91,21 +132,32 @@ const Header = () => {
           </Paper>
         </Grid> */}
 
-        <Grid container  item xs={6} sm={4}    lg={2} justify={justifyItems} alignItems={"center"}>
+        <Grid
+          container
+          item
+          xs={6}
+          sm={4}
+          lg={2}
+          justify={justifyItems}
+          alignItems={"center"}
+        >
           <Paper className={`${materialUiClasses.paper} ${classes.boxShadow}`}>
-              <Button className={`${materialUiClasses.button} ${classes.decoration}`}  variant="contained" >
+            <Button
+              className={`${materialUiClasses.button} ${classes.decoration}`}
+              variant="contained"
+            >
               <NavLink
                 to={`/login`}
                 draggable={false}
                 className={`${materialUiClasses.button} ${classes.decoration}`}
               >
-              Sign In
-            </NavLink>
+                Sign In
+              </NavLink>
             </Button>
           </Paper>
         </Grid>
       </Grid>
-  </div>
+    </div>
   );
 };
 
